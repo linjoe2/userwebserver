50cdc5cffc7d049b03d2578eea41f43c47e9a59c
[linjoe2] <[linjoe_is@live.nl]>
2016-02-08 14:23:29 +0100

    ajax working

--- src/app.js
+++ src/app.js
@@ -37,57 +37,6 @@ app.post('/searchRequest', function(req, resp) {
 	};
 });
 
-
-// vraag komt binnen
-// gaat door if loop heen op het voorkomt in de voornaam
-
-// gaat door een if loop heen op het voorkomen in de achternaam
-// verwijst terug op zelfde pagina
-
-
-// app.post('/searchRequest2', function(req, res) {
-// 	for (var i = 0; i <= users.length; i++) {
-// 		var usr = users[i].firstname + users[i].lastname
-// 		console.log(usr) 
-// 		if (usr.indexOf(req.body.Name) === -1) {
-// 			console.log('empty')
-// 		} else {
-// 			res.send(usr)
-// 			}
-// 	};
-// })
-
-
-
-// app.post('/searchRequest2',
-// 	bodyParser.urlencoded({
-// 		extended: true
-// 	}),
-// 	function(request, response) {
-// 		console.log(request.body.name)
-// 		var b = 0
-// 		var usr = request.body.name
-// 		for (var i = users.length - 1; i >= 0; i--) {
-// 			if (users[i].firstname === usr) {
-// 				response.send('Firstname: ' + JSON.stringify(users[i].firstname) + '<br>' + "Lastname: " + JSON.stringify(users[i].lastname) + '<br>' + 'Email: ' + JSON.stringify(users[i].email));
-// 				console.log(users[i].firstname + ' got searched')
-// 				var b = 1
-// 			} else {
-// 				if (users[i].lastname === usr) {
-// 					response.send('Firstname: ' + JSON.stringify(users[i].firstname) + '<br>' + "Lastname: " + JSON.stringify(users[i].lastname) + '<br>' + 'Email: ' + JSON.stringify(users[i].email));
-// 					console.log(users[i].firstname + ' got searched')
-// 					var b = 1
-// 				}
-// 				if (i === 0 && b === 0) {
-// 					response.send('user not found');
-// 					console.log('user not found')
-// 					var b = 1
-// 				}
-// 			}
-// 		};
-// 	}
-// );
-
 app.post('/searchRequest2',
 	bodyParser.urlencoded({
 		extended: true
@@ -95,24 +44,27 @@ app.post('/searchRequest2',
 	function(request, response) {
 		var b = 0
 		var usr = request.body.name
-		console.log('request= ' + usr)
-		for (var i = 0; i <= users.length; i++) {
-			var usrt = users[i].firstname + ' ' + users[i].lastname
-			console.log('name= ' + usrt)
-			console.log('index= ' + usrt.indexOf(usr))
-			if (usrt.indexOf(usr) === -1) {
-				var b = 0
-			} else {
-				response.send('Firstname: ' + JSON.stringify(usrt) + '<br>' + 'Email: ' + JSON.stringify(users[i].email));
-				console.log(usrt + ' got searched')
-				var b = 1
-			}
-				if (i === 0 && b === 0) {
+		if (usr.length === 0) {
+			console.log('blank field')
+			b = 1
+		} else {
+			console.log('request= ' + usr)
+			for (var i = 0; i <= users.length - 1; i++) {
+				var usrt = users[i].firstname + ' ' + users[i].lastname
+				if (usrt.indexOf(usr) === -1) {
+				} else {
+					response.send('Name: ' + JSON.stringify(usrt) + '<br>' + 'Email: ' + JSON.stringify(users[i].email));
+					console.log(usrt + ' got searched')
+					var b = 1
+				}
+				if (i === users.length - 1 && b === 0) {
 					response.send('user not found');
 					console.log('user not found')
 					var b = 1
 				};
-		};
+			};
+			console.log('----')
+		}
 	}
 );
 
